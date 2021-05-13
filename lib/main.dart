92a995eec4f188ce7fae8b4a49589d83beaa6d69
smchoi66 <smchoi66@gmail.com>
2021-05-13 18:39:31 +0900

    rev2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -30,37 +28,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('SliverFillRemaining')),
-      body: CustomScrollView(
-        slivers: [
-          SliverList(
-            delegate: SliverChildListDelegate(const [
-              ListTile(title: Text('First item')),
-              ListTile(title: Text('Second item')),
-              ListTile(title: Text('Third item')),
-              ListTile(title: Text('Fourth item')),
-            ]),
-          ),
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: Container(
-              color: Colors.yellowAccent,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  FlutterLogo(size: 200),
-                  SizedBox(),
-                  Text(
-                    'This is some longest text that should be centered '
-                    'together with the logo',
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+      body: const Text('text'),
     );
   }
 }
