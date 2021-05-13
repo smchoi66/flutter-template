a72af5bcf44de7c95111c63f95049df6e5185c48
smchoi66 <smchoi66@gmail.com>
2021-05-13 18:27:15 +0900

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,7 +10,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Template',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -27,10 +30,37 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Template'),
+      appBar: AppBar(title: Text('SliverFillRemaining')),
+      body: CustomScrollView(
+        slivers: [
+          SliverList(
+            delegate: SliverChildListDelegate(const [
+              ListTile(title: Text('First item')),
+              ListTile(title: Text('Second item')),
+              ListTile(title: Text('Third item')),
+              ListTile(title: Text('Fourth item')),
+            ]),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Container(
+              color: Colors.yellowAccent,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  FlutterLogo(size: 200),
+                  SizedBox(),
+                  Text(
+                    'This is some longest text that should be centered '
+                    'together with the logo',
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
