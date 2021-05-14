a29ffeeb8f076e3f913e071caaf73152b36c6944
smchoi66 <32591695+smchoi66@users.noreply.github.com>
2021-05-14 15:38:55 +0900

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -25,11 +28,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<List<User>> getUserData() async {
+    final response =
+        await http.get(Uri.https('jsonplaceholder.typicode.com', 'users'));
+    final jsonData = jsonDecode(response.body);
+    final List<User> users = [];
+    if (jsonData != null) {
+      for (final u in jsonData) {
+        final User user = User(
+            u['name'] as String, u['email'] as String, u['username'] as String);
+        users.add(user);
+      }
+    }
+    return users;
+  }
+
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
     return Scaffold(
-      body: const Text('text'),
+      body: Center(
+        child: FutureBuilder<List<User>>(
+          future: getUserData(),
+          builder: (context, snapshot) {
+            if (snapshot.data == null) {
+              return const CircularProgressIndicator();
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (_, int index) {
+                  return ListTile(
+                    title: Text(snapshot.data![index].name),
+                    subtitle: Text(snapshot.data![index].userName),
+                    trailing: Text(snapshot.data![index].userName),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
     );
   }
+}
+
+class User {
+  final String name;
+  final String email;
+  final String userName;
+
+  User(this.name, this.email, this.userName);
 }
